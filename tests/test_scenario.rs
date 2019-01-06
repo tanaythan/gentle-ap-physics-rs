1ef63228cad234cd1a3c7264fb467c092d91583c
Krishnan Rajiyah <krajiyah@gmail.com>
2019-01-06 13:26:55 -0800

    working on scenario test

--- tests/test_scenario.rs
+++ tests/test_scenario.rs
@@ -1,5 +1,21 @@
+extern crate gentle_ap_physics_rs;
+
+use gentle_ap_physics_rs::entity;
+
 #[test]
 fn test_scenario_1() {
-    // TODO: implement me
-    assert_eq!(0.1, 0.1);
+    let state = entity::Vector3::new(1.0, 2.0, 3.0);
+    let sphereA = entity::sphere::Sphere::new(state.clone(), 1.0, 2.0, 4.0);
+    let sphereB = entity::sphere::Sphere::new(state.clone(), 1.0, 2.0, 4.0);
+    let plane = entity::plane::Plane::new(state.clone(), 1.0, 2.0, 4.0);
+    let all_entities: Vec<Box<entity::BaseEntity>> = Vec::new();
+    all_entities.push(Box::new(plane));
+    all_entities.push(Box::new(sphereA));
+    all_entities.push(Box::new(sphereB));
+
+    let state = entity::worldstate::WorldState::new(all_entities);
+    let mut t = 1.0;
+    let dt = 1.0;
+    
+    state.update_entities(t, dt);
 }

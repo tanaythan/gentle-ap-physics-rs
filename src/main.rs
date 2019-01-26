567f7ddde266b732fd2bfc17c17de383e2008f5f
Krishnan Rajiyah <krajiyah@gmail.com>
2019-01-25 20:35:36 -0800

    feat: add spheres to main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,28 @@ mod util;
 use std::collections::HashMap;
 use util::vector3::Vector3;
 use util::time::duration_to_s;
+use entity::plane::Plane;
+use entity::sphere::Sphere;
 
 /* We can define sample constants here */
 const dt: f32 = 0.1;
 
 fn main() {
-    //Initialize our sample planes and spheres
+    //Initialize our sample entities
     let state = Vector3::new(1.0, 2.0, 3.0);
-    let plane = entity::plane::Plane::new(state.clone(), 1.0, 2.0, 4.0);
+    let init_pos_1 = Vector3::new(2.0, 2.0, 2.0);
+    let init_pos_2 = Vector3::new(5.0, 2.0, 2.0);
+    let v1 = Vector3::new(1.0, 0.0, 0.0);
+    let v2 = Vector3::new(-1.0, 0.0, 0.0);
+    let m = 1.0;
+    let r = 1.0;
+    let plane = Plane::new(state.clone(), 1.0, 2.0, 4.0);
+    let sphere1 = Sphere::new(init_pos_1, m, r, v1);
+    let sphere2 = Sphere::new(init_pos_2, m, r, v2);
     let mut all_entities: HashMap<String, Box<entity::BaseEntity>> = HashMap::new();
     all_entities.insert(String::from("Plane1"), Box::new(plane));
+    all_entities.insert(String::from("Sphere1"), Box::new(sphere1));
+    all_entities.insert(String::from("Sphere2"), Box::new(sphere2));
 
     //Initialize sample world state
     let mut state = entity::worldstate::WorldState::new_with_map(all_entities);

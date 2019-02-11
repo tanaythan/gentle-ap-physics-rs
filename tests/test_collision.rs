65df6add9571d7d5cba361858568aeda7a9dc9b7
Krishnan Rajiyah <krajiyah@gmail.com>
2019-02-10 18:56:59 -0800

    chore: add integration test for additional forces and collision

--- tests/test_collision.rs
+++ tests/test_collision.rs
@@ -100,3 +100,48 @@ fn test_complex_sphere_collision() {
 
     assert_eq!(true, sphere1Box.is_collided(sphere2Box));
 }
+
+#[test]
+fn test_collision_with_additional_forces() {
+    let state = Vector3::new(1.0, 1.0, 1.0);
+    let init_pos_1 = Vector3::new(2.0, 2.0, 2.0);
+    let init_pos_2 = Vector3::new(5.0, 2.0, 2.0);
+    let v1 = Vector3::new(1.0, 0.0, 0.0);
+    let v2 = Vector3::new(-1.0, 0.0, 0.0);
+    let f1Right = Vector3::new(1.0, 0.0, 0.0);
+    let f2Right = Vector3::new(1.0, 0.0, 0.0);
+    let f1Deep = Vector3::new(0.0, 0.0, -1.0);
+    let f2Deep = Vector3::new(0.0, 0.0, -1.0);
+    let m = 1.0;
+    let r = 1.0;
+    let mut sphere1 = Sphere::new(String::from("Sphere1"), init_pos_1, m, r, v1);
+    sphere1.apply_force(f1Right);
+    sphere1.apply_force(f1Deep);
+    let mut sphere2 = Sphere::new(String::from("Sphere2"), init_pos_2, m, r, v2);
+    sphere2.apply_force(f2Right);
+    sphere2.apply_force(f2Deep);
+    let plane = Plane::new(String::from("Plane1"), state, 1.0, 2.0, 4.0);
+    let plane1_key = String::from("Plane1");
+    let sphere1_key = String::from("Sphere1");
+    let sphere2_key = String::from("Sphere2");
+    let mut state = WorldState::new();
+    state.add("Plane1", Entity::Plane(plane));
+    state.add("Sphere1", Entity::Sphere(sphere1));
+    state.add("Sphere2", Entity::Sphere(sphere2));
+    let dt = 1.0;
+
+    // Should not collide before starting updates
+    let mut sphere1Box = state.get(String::from("Sphere1"));
+    let mut sphere2Box = state.get(String::from("Sphere2"));
+
+    assert_eq!(false, sphere1Box.is_collided(sphere2Box));
+
+    // After updates should collide
+    state.step(dt);
+    state.step(dt);
+
+    let mut sphere1Box = state.get(String::from("Sphere1"));
+    let mut sphere2Box = state.get(String::from("Sphere2"));
+
+    assert_eq!(true, sphere1Box.is_collided(sphere2Box));
+}

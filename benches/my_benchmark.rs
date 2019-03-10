df24bafadbfc0e5dc2baa7b2fdfdc69240e3d81b
Gabriel Gardner <gabrielgardner@gabriels-mbp-2.lan>
2019-03-10 12:48:01 -0400

    Added basic benchmarking capability

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,16 @@
+#[macro_use]
+extern crate criterion;
+extern crate gentle_ap_physics_rs;
+
+use criterion::Criterion;
+
+use gentle_ap_physics_rs::util::vector3::Vector3;
+
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let x = Vector3::new (1.0, 1.0, 1.0);
+    c.bench_function("vec_magnitude", |b| b.iter(|| x.magnitude ()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
